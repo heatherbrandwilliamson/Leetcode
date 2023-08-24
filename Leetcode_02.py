8deb8b940daa17385ec3f2b18b69d8ef1f713346
heatherbrandwilliamson <heather.brandwilliamson@gmail.com>
2023-08-24 15:26:16 +0100

    1 & 2 solved

--- Leetcode_02.py
+++ Leetcode_02.py
@@ -1,3 +1,16 @@
 class Solution:
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len_gcd = gcd(len(str1), len(str2))
+        common_substring = str1[:len_gcd]
+
+        if common_substring * (len(str1) // len_gcd) == str1 and \
+            common_substring * (len(str2) // len_gcd) == str2:
+            return common_substring
+        else:
+            return ""
 
